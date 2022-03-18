8146a4d248e5771ccc1ac1de972dbfc876b956e1
gabrillukita1 <gabrillukita@gmail.com>
2022-03-18 20:26:13 +0700

    added .env and .env local

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const giphy_secret_key = process.env.REACT_APP_GIPHY_KEY;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { console.log(giphy_secret_key) }
+      <p>ohaa</p>
+      {/* { console.log("hai") } */}
     </div>
   );
 }
